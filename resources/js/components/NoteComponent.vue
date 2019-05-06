ec3271a1e38d17aa3aad8f9af10b22a25c1ec260
maythu98 <maythuaung1998.mm@gmail.com>
2019-05-06 12:03:41 +0630

    expandtextarea

---
+++ resources/js/components/NoteComponent.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 shadow p-3 mb-5 bg-white rounded">
+				<div class="form-group">
+					<input type="text" name="title" class="form-control" @click="createList" placeholder="Title">	
+				</div>
+				<div class="form-group">
+					<textarea type="text" rows="1" name="note" aria-label="note" class="form-control" placeholder="Take a note" v-show="formDetail"></textarea>
+				</div>
+				<button @click="SaveNote"  v-show="NoteVisible"> Close </button>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				formDetail: false,
+				RowNumber: 2,
+			}
+		},
+		methods: {
+			createList() {
+				this.formDetail = true;
+			},
+			NotDetail() {
+				this.formDetail = false;
+			},
+            SaveNote() {
+
+            },
+			expandNoteArea() {
+				this.RowNumber +=1;
+			}
+
+		},
+
+		mounted() {
+		}
+
+	};
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,6 +93,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+                <example-component></example-component>
             </div>
         </div>
     </body>
